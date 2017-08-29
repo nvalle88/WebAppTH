663726eab4c253f68de8c850762d25d74f496619
carlosavila890 <carlos.avila8909@gmail.com>
2017-08-29 10:13:05 -0400

    Funciones para inicializar los plugins Touchspin y Select2 (ComboBox)

--- WebAppTH/bd.webappth.web/wwwroot/js/site.js
+++ WebAppTH/bd.webappth.web/wwwroot/js/site.js
@@ -1,1 +1,22 @@
-﻿// Write your Javascript code.
+﻿function Init_Touchspin()
+{
+    $('.touchspin_tasa').TouchSpin({
+        min: 0,
+        max: 99999999999,
+        decimals: 2,
+        prefix: '$',
+        step: 0.01,
+        buttondown_class: 'btn btn-primary',
+        buttonup_class: 'btn btn-primary'
+    });
+}
+
+function Init_Select2()
+{
+    $('select').select2({
+        theme: 'classic',
+        allowClear: false,
+        placeholder: 'Seleccione...',
+        language: 'es'
+    });
+}
