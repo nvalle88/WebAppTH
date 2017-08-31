093f6c2291c078196c5e45cd2cae7eae72b12743
Adriana Cristina Espinosa Román <aespinosa@digitalstrategy.com.ec>
2017-08-31 14:06:05 -0500

    Cambios en controlador y vistas del cliente desde la F a M

--- WebAppTH/bd.webappth.entidades/Negocio/FrecuenciaAplicacion.cs
+++ WebAppTH/bd.webappth.entidades/Negocio/FrecuenciaAplicacion.cs
@@ -9,7 +9,7 @@ namespace bd.webappth.entidades.Negocio
         public int IdFrecuenciaAplicacion { get; set; }
 
         [Required(ErrorMessage = "Debe introducir {0}")]
-        [Display(Name = "Frecuancia de aplicación:")]
+        [Display(Name = "Frecuencia de la Aplicación:")]
         [StringLength(20, MinimumLength = 2, ErrorMessage = "El {0} no puede tener más de {1} y menos de {2}")]
         public string Nombre { get; set; }
 
