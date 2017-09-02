07bb1556294a9b90666c66be755d9e38d19078eb
Yasmani Palmero Valdivia <yasmany.07@gmail.com>
2017-09-02 02:52:31 +0200

    Controladores MVC
    UnidadMedida
    FondoFinanciamiento
    MotivoAsiento
    TipoArticulo

---
+++ WebAppTH/bd.webappth.web/Controllers/MVC/FondoFinanciamientoController.cs
@@ -0,0 +1,212 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using bd.webapprm.servicios.Interfaces;
+using bd.webapprm.entidades.Utils;
+using bd.log.guardar.Servicios;
+using bd.log.guardar.ObjectTranfer;
+using bd.log.guardar.Enumeradores;
+using Newtonsoft.Json;
+using bd.webapprm.entidades;
+using bbd.webapprm.servicios.Enumeradores;
+
+namespace bd.webapprm.web.Controllers.MVC
+{
+    public class FondoFinanciamientoController : Controller
+    {
+        private readonly IApiServicio apiServicio;
+
+        public FondoFinanciamientoController(IApiServicio apiServicio)
+        {
+            this.apiServicio = apiServicio;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var lista = new List<FondoFinanciamiento>();
+            try
+            {
+                lista = await apiServicio.Listar<FondoFinanciamiento>(new Uri(WebApp.BaseAddress)
+                                                                    , "/api/FondoFinanciamiento/ListarFondoFinanciamiento");
+                return View(lista);
+            }
+            catch (Exception ex)
+            {
+                await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                {
+                    ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                    Message = "Listando Fondos de Financiamientos",
+                    ExceptionTrace = ex,
+                    LogCategoryParametre = Convert.ToString(LogCategoryParameter.NetActivity),
+                    LogLevelShortName = Convert.ToString(LogLevelParameter.ERR),
+                    UserName = "Usuario APP webappth"
+                });
+                return BadRequest();
+            }
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(FondoFinanciamiento fondoFinanciamiento)
+        {
+            Response response = new Response();
+            try
+            {
+                response = await apiServicio.InsertarAsync(fondoFinanciamiento,
+                                                             new Uri(WebApp.BaseAddress),
+                                                             "/api/FondoFinanciamiento/InsertarFondoFinanciamiento");
+                if (response.IsSuccess)
+                {
+
+                    var responseLog = await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                    {
+                        ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                        ExceptionTrace = null,
+                        Message = "Se ha creado un Fondo de Financiamiento",
+                        UserName = "Usuario 1",
+                        LogCategoryParametre = Convert.ToString(LogCategoryParameter.Create),
+                        LogLevelShortName = Convert.ToString(LogLevelParameter.ADV),
+                        EntityID = string.Format("{0} {1}", "Fondo de Financiamiento:", fondoFinanciamiento.IdFondoFinanciamiento),
+                    });
+
+                    return RedirectToAction("Index");
+                }
+
+                ViewData["Error"] = response.Message;
+                return View(fondoFinanciamiento);
+
+            }
+            catch (Exception ex)
+            {
+                await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                {
+                    ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                    Message = "Creando Fondo de Financiamiento",
+                    ExceptionTrace = ex,
+                    LogCategoryParametre = Convert.ToString(LogCategoryParameter.Create),
+                    LogLevelShortName = Convert.ToString(LogLevelParameter.ERR),
+                    UserName = "Usuario APP WebAppTh"
+                });
+
+                return BadRequest();
+            }
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(id))
+                {
+                    var respuesta = await apiServicio.SeleccionarAsync<Response>(id, new Uri(WebApp.BaseAddress),
+                                                                  "/api/FondoFinanciamiento");
+
+
+                    respuesta.Resultado = JsonConvert.DeserializeObject<FondoFinanciamiento>(respuesta.Resultado.ToString());
+                    if (respuesta.IsSuccess)
+                    {
+                        return View(respuesta.Resultado);
+                    }
+
+                }
+
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, FondoFinanciamiento fondoFinanciamiento)
+        {
+            Response response = new Response();
+            try
+            {
+                if (!string.IsNullOrEmpty(id))
+                {
+                    response = await apiServicio.EditarAsync(id, fondoFinanciamiento, new Uri(WebApp.BaseAddress),
+                                                                 "/api/FondoFinanciamiento");
+
+                    if (response.IsSuccess)
+                    {
+                        await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                        {
+                            ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                            EntityID = string.Format("{0} : {1}", "Fondo de Financiamiento", id),
+                            LogCategoryParametre = Convert.ToString(LogCategoryParameter.Edit),
+                            LogLevelShortName = Convert.ToString(LogLevelParameter.ADV),
+                            Message = "Se ha actualizado un registro Fondo de Financiamiento",
+                            UserName = "Usuario 1"
+                        });
+
+                        return RedirectToAction("Index");
+                    }
+
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                {
+                    ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                    Message = "Editando un Fondo de Financiamiento",
+                    ExceptionTrace = ex,
+                    LogCategoryParametre = Convert.ToString(LogCategoryParameter.Edit),
+                    LogLevelShortName = Convert.ToString(LogLevelParameter.ERR),
+                    UserName = "Usuario APP webappth"
+                });
+
+                return BadRequest();
+            }
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+
+            try
+            {
+                var response = await apiServicio.EliminarAsync(id, new Uri(WebApp.BaseAddress)
+                                                               , "/api/FondoFinanciamiento");
+                if (response.IsSuccess)
+                {
+                    await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                    {
+                        ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                        EntityID = string.Format("{0} : {1}", "Fondo de Financiamiento", id),
+                        Message = "Registro eliminado",
+                        LogCategoryParametre = Convert.ToString(LogCategoryParameter.Delete),
+                        LogLevelShortName = Convert.ToString(LogLevelParameter.ADV),
+                        UserName = "Usuario APP webappth"
+                    });
+                    return RedirectToAction("Index");
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                {
+                    ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                    Message = "Eliminar Fondo de Financiamiento",
+                    ExceptionTrace = ex,
+                    LogCategoryParametre = Convert.ToString(LogCategoryParameter.Delete),
+                    LogLevelShortName = Convert.ToString(LogLevelParameter.ERR),
+                    UserName = "Usuario APP webappth"
+                });
+
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ WebAppTH/bd.webappth.web/Controllers/MVC/MotivoAsientoController.cs
@@ -0,0 +1,216 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using bd.webapprm.servicios.Interfaces;
+using bd.webapprm.entidades.Utils;
+using bd.log.guardar.Servicios;
+using bd.log.guardar.ObjectTranfer;
+using bd.log.guardar.Enumeradores;
+using Newtonsoft.Json;
+using bd.webapprm.entidades;
+using bbd.webapprm.servicios.Enumeradores;
+
+namespace bd.webapprm.web.Controllers.MVC
+{
+    public class MotivoAsientoController : Controller
+    {
+        private readonly IApiServicio apiServicio;
+
+        public MotivoAsientoController(IApiServicio apiServicio)
+        {
+            this.apiServicio = apiServicio;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var lista = new List<MotivoAsiento>();
+            try
+            {
+                lista = await apiServicio.Listar<MotivoAsiento>(new Uri(WebApp.BaseAddress)
+                                                                    , "/api/MotivoAsiento/ListarMotivoAsiento");
+                return View(lista);
+            }
+            catch (Exception ex)
+            {
+                await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                {
+                    ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                    Message = "Listando Motivos de Asientos",
+                    ExceptionTrace = ex,
+                    LogCategoryParametre = Convert.ToString(LogCategoryParameter.NetActivity),
+                    LogLevelShortName = Convert.ToString(LogLevelParameter.ERR),
+                    UserName = "Usuario APP webappth"
+                });
+                return BadRequest();
+            }
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            ViewData["IdConfiguracionContabilidad"] = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(await apiServicio.Listar<MotivoAsiento>(new Uri(WebApp.BaseAddress), "/api/ConfiguracionContabilidad/ListarConfiguracionContabilidad"), "IdConfiguracionContabilidad", "IdConfiguracionContabilidad");
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(MotivoAsiento motivoAsiento)
+        {
+            Response response = new Response();
+            try
+            {
+                response = await apiServicio.InsertarAsync(motivoAsiento,
+                                                             new Uri(WebApp.BaseAddress),
+                                                             "/api/MotivoAsiento/InsertarMotivoAsiento");
+                if (response.IsSuccess)
+                {
+
+                    var responseLog = await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                    {
+                        ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                        ExceptionTrace = null,
+                        Message = "Se ha creado un Motivo de Asiento",
+                        UserName = "Usuario 1",
+                        LogCategoryParametre = Convert.ToString(LogCategoryParameter.Create),
+                        LogLevelShortName = Convert.ToString(LogLevelParameter.ADV),
+                        EntityID = string.Format("{0} {1}", "Motivo de Asiento:", motivoAsiento.IdMotivoAsiento),
+                    });
+
+                    return RedirectToAction("Index");
+                }
+
+                ViewData["Error"] = response.Message;
+                return View(motivoAsiento);
+
+            }
+            catch (Exception ex)
+            {
+                await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                {
+                    ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                    Message = "Creando Motivo de Asiento",
+                    ExceptionTrace = ex,
+                    LogCategoryParametre = Convert.ToString(LogCategoryParameter.Create),
+                    LogLevelShortName = Convert.ToString(LogLevelParameter.ERR),
+                    UserName = "Usuario APP WebAppTh"
+                });
+
+                return BadRequest();
+            }
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(id))
+                {
+                    var respuesta = await apiServicio.SeleccionarAsync<Response>(id, new Uri(WebApp.BaseAddress),
+                                                                  "/api/MotivoAsiento");
+
+
+                    respuesta.Resultado = JsonConvert.DeserializeObject<MotivoAsiento>(respuesta.Resultado.ToString());
+                    ViewData["IdConfiguracionContabilidad"] = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(await apiServicio.Listar<MotivoAsiento>(new Uri(WebApp.BaseAddress), "/api/ConfiguracionContabilidad/ListarConfiguracionContabilidad"), "IdConfiguracionContabilidad", "IdConfiguracionContabilidad");
+
+                    if (respuesta.IsSuccess)
+                    {
+                        return View(respuesta.Resultado);
+                    }
+
+                }
+
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, MotivoAsiento motivoAsiento)
+        {
+            Response response = new Response();
+            try
+            {
+                if (!string.IsNullOrEmpty(id))
+                {
+                    response = await apiServicio.EditarAsync(id, motivoAsiento, new Uri(WebApp.BaseAddress),
+                                                                 "/api/MotivoAsiento");
+
+                    if (response.IsSuccess)
+                    {
+                        await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                        {
+                            ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                            EntityID = string.Format("{0} : {1}", "Motivo de Asiento", id),
+                            LogCategoryParametre = Convert.ToString(LogCategoryParameter.Edit),
+                            LogLevelShortName = Convert.ToString(LogLevelParameter.ADV),
+                            Message = "Se ha actualizado un registro Motivo de Asiento",
+                            UserName = "Usuario 1"
+                        });
+
+                        return RedirectToAction("Index");
+                    }
+
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                {
+                    ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                    Message = "Editando un Motivo de Asiento",
+                    ExceptionTrace = ex,
+                    LogCategoryParametre = Convert.ToString(LogCategoryParameter.Edit),
+                    LogLevelShortName = Convert.ToString(LogLevelParameter.ERR),
+                    UserName = "Usuario APP webappth"
+                });
+
+                return BadRequest();
+            }
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+
+            try
+            {
+                var response = await apiServicio.EliminarAsync(id, new Uri(WebApp.BaseAddress)
+                                                               , "/api/MotivoAsiento");
+                if (response.IsSuccess)
+                {
+                    await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                    {
+                        ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                        EntityID = string.Format("{0} : {1}", "Motivo de Asiento", id),
+                        Message = "Registro eliminado",
+                        LogCategoryParametre = Convert.ToString(LogCategoryParameter.Delete),
+                        LogLevelShortName = Convert.ToString(LogLevelParameter.ADV),
+                        UserName = "Usuario APP webappth"
+                    });
+                    return RedirectToAction("Index");
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                await GuardarLogService.SaveLogEntry(new LogEntryTranfer
+                {
+                    ApplicationName = Convert.ToString(Aplicacion.WebAppRM),
+                    Message = "Eliminar Motivo de Asiento",
+                    ExceptionTrace = ex,
+                    LogCategoryParametre = Convert.ToString(LogCategoryParameter.Delete),
+                    LogLevelShortName = Convert.ToString(LogLevelParameter.ERR),
+                    UserName = "Usuario APP webappth"
+                });
+
+                return BadRequest();
+            }
+        }
+    }
+}
