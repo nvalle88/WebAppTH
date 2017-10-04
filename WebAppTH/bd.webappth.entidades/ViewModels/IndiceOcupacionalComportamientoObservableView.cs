482c128d548869c1499c9c5132333d89b5742ff3
Paul Cevallos <pcevallos@digitalstrategy.com.ec>
2017-10-04 12:42:02 -0500

    Se modificó funcionalidad para agregar un nuevo IndiceOcupacionalComportamientosObservables y se optimizó el código poniendo el modal en una sola clase y el javascript que ejecuta la acción.

---
+++ WebAppTH/bd.webappth.entidades/ViewModels/IndiceOcupacionalComportamientoObservableView.cs
@@ -0,0 +1,13 @@
+﻿using bd.webappth.entidades.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bd.webappth.entidades.ViewModels
+{
+  public  class IndiceOcupacionalComportamientoObservableView
+    {
+        public int IdIndiceOcupacional { get; set; }
+        public List<ComportamientoObservable> ComportamientoObservables { get; set; }
+    }
+}
