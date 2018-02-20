fe24eff7e0d557b3c5950c1453e81f9a4d48204a
Néstor Valle <nestorvalle880416@gmail.com>
2018-02-20 02:21:46 -0500

    Se válidan los campos de agregar empleado

---
+++ WebAppTH/bd.webappth.entidades/Negocio/Ejemplo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bd.webappth.entidades.Negocio
+{
+    public partial class Ejemplo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime Fecha { get; set; }
+        public int? Edad { get; set; }
+    }
+}
