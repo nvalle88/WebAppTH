cf504b529fc20feeb646b09377b7bbe6fbe13ab9
jcatota <jcatota@digitalstrategy.com.ec>
2018-04-20 16:11:29 -0500

    Creado proceso Induccion, aplicado estilo default de tablas en fichas médicas, activacionespersonalTTHH,AprobarMateralesInduccion, Induccion, SituacionPropuesta

--- WebAppTH/bd.webappth.entidades/Negocio/MaterialInduccion.cs
+++ WebAppTH/bd.webappth.entidades/Negocio/MaterialInduccion.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace bd.webappth.entidades.Negocio
 {
     public partial class MaterialInduccion
     {
         public int IdMaterialInduccion { get; set; }
+
+        [Display(Name = "Título")]
         public string Titulo { get; set; }
+
+        [Display(Name = "Descripción")]
         public string Descripcion { get; set; }
+        
         public string Url { get; set; }
     }
 }
--- WebAppTH/bd.webappth.web/Controllers/MVC/InduccionController.cs
+++ WebAppTH/bd.webappth.web/Controllers/MVC/InduccionController.cs
@@ -2,15 +2,60 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using bd.webappth.entidades.Utils;
+using bd.webappth.entidades.ViewModels;
+using bd.webappth.servicios.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bd.webappth.web.Controllers.MVC
 {
     public class InduccionController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IApiServicio apiServicio;
+
+        public InduccionController(IApiServicio apiServicio)
         {
-            return View();
+            this.apiServicio = apiServicio;
+
+        }
+
+        private void InicializarMensaje(string mensaje)
+
+        {
+
+            if (mensaje == null)
+            {
+                mensaje = "";
+            }
+
+            ViewData["Error"] = mensaje;
+        }
+
+        public async Task<IActionResult> Index(string mensaje)
+        {
+            InicializarMensaje(mensaje);
+
+            var lista = new List<InduccionViewModel>();
+            
+
+            try
+            {
+                lista = await apiServicio.Listar<InduccionViewModel>(new Uri(WebApp.BaseAddress)
+                         , "api/Induccion/ListarEstadoInduccionEmpleados");
+
+                
+                return View(lista);
+
+
+            }
+            catch (Exception ex)
+            {
+                mensaje = Mensaje.Excepcion;
+
+                return View(lista);
+
+            }
         }
     }
 }
