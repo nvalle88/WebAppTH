aeb8033342b12e7ca9f06c09ac7598cad07aa712
Alex Adriano <aadriano@digitalstrategy.com.ec>
2018-04-25 17:23:24 -0500

    Index y Evaluar hasta actividades Evaluacion Desempeño

---
+++ WebAppTH/bd.webappth.entidades/ViewModels/ViewModelEvaluador.cs
@@ -0,0 +1,26 @@
+﻿using bd.webappth.entidades.Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace bd.webappth.entidades.ViewModels
+{
+    public class ViewModelEvaluador
+    {
+        public int IdEmpleado { get; set; }
+        [Display(Name = "Apellido y Nombres:")]
+        public string NombreApellido { get; set; }
+        [Display(Name = "Puesto")]
+        public string Puesto { get; set; }
+        [Display(Name = "Titulo o Profesión:")]
+        public string Titulo { get; set; }
+        [Display(Name = "Apellido y Nombres Jefe Inmediato:")]
+        public string DatosJefe { get; set; }
+        public DateTime Desde { get; set; }
+        public DateTime Hasta { get; set; }
+        public string NombreUsuario { get; set; }
+        public List<ActividadesEsenciales> ListaActividad { get; set; }
+
+    }
+}
