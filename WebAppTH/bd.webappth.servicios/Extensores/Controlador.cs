a540c432b6567a82a0c4ce3ebead82921092a762
jcatota <jcatota@digitalstrategy.com.ec>
2018-05-09 19:42:47 -0500

    Terminado Proceso MovimientoPersonalTTHH (funcional)

--- WebAppTH/bd.webappth.servicios/Extensores/Controlador.cs
+++ WebAppTH/bd.webappth.servicios/Extensores/Controlador.cs
@@ -54,5 +54,22 @@ namespace bd.webappth.servicios.Extensores
 
             return controlador.RedirectToAction(nombreVista, NombreControlador, parametros);
         }
+
+        /// <summary>
+        /// Redirecciona a una Vista en el otro Controlador con parámetros y permite controlar el tiempo que dura el mensaje
+        /// </summary>
+        /// <param name="controlador"></param>
+        /// <param name="Controlador">Controlador al que se desa hacer la redirección</param>
+        /// <param name="nombreVista">Acción a la que se desea redireccionar</param>
+        /// <param name="parametros">Parametros que recive la acción que estamos invocando</param>
+        /// <param name="msg">Mensaje que saldrá en la parte superior derecha de la pantalla cuando cargue la Vista a la que se redirecciona.</param>
+        /// <returns></returns>
+        public static IActionResult RedireccionarMensajeTime(this Controller controlador, string NombreControlador, string nombreVista, object parametros, string msg = null)
+        {
+            if (!String.IsNullOrEmpty(msg))
+                controlador.TempData["MensajeTimer"] = msg;
+
+            return controlador.RedirectToAction(nombreVista, NombreControlador, parametros);
+        }
     }
 }
