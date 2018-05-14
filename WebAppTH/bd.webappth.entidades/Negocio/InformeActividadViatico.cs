145fe84e19230786cfc7554fd132b0b727e9f2ff
Alex Adriano <aadriano@digitalstrategy.com.ec>
2018-05-14 17:41:50 -0500

    Cambios en Itinerario Viatico (Reliquidacion informe), solicitud de viaticos

---
+++ WebAppTH/bd.webappth.entidades/Negocio/InformeActividadViatico.cs
@@ -0,0 +1,11 @@
+﻿namespace bd.webappth.entidades.Negocio
+{
+    public partial class InformeActividadViatico
+    {
+        public int IdInformeActividad { get; set; }
+        public int? IdItinerarioViatico { get; set; }
+        public string Descripcion { get; set; }
+
+        public virtual ItinerarioViatico ItinerarioViatico { get; set; }
+    }
+}
---
+++ WebAppTH/bd.webappth.entidades/ViewModels/ReliquidacionViaticoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using bd.webappth.entidades.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bd.webappth.entidades.ViewModels
+{
+    public class ReliquidacionViaticoViewModel
+    {
+        public SolicitudViatico SolicitudViatico { get; set; }
+        public List<SolicitudTipoViatico> SolicitudTipoViatico { get; set; }
+        public List<ReliquidacionViatico> ReliquidacionViatico { get; set; }
+        public ListaEmpleadoViewModel ListaEmpleadoViewModel { get; set; }
+        public List<FacturaViatico> FacturaViatico { get; set; }
+        public int IdItinerarioViatico { get; set; }
+        public int IdSolicitudViatico { get; set; }
+        public string Descripcion { get; set; }
+        //public List<String> ViaticosSeleccionados { get; set; }
+    }
+}
