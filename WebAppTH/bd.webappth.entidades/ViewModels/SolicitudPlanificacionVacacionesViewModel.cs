4c31106648b4340215b03fe504286d54bf032ade
jcatota <jcatota@digitalstrategy.com.ec>
2018-05-21 15:44:27 -0500

    SolicitudPlanificacionVacaciones (Empleado) funcionando, falta proceso Aprobación

---
+++ WebAppTH/bd.webappth.entidades/ViewModels/SolicitudPlanificacionVacacionesViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace bd.webappth.entidades.ViewModels
+{
+    public class SolicitudPlanificacionVacacionesViewModel
+    {
+        public DatosBasicosEmpleadoViewModel DatosBasicosEmpleadoViewModel { get; set; }
+
+        public int IdSolicitudPlanificacionVacaciones { get; set; }
+
+        [Display(Name = "Fecha")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime FechaSolicitud { get; set; }
+
+        [Display(Name = "Solicita desde")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime FechaDesde { get; set; }
+
+        [Display(Name = "Solicita hasta")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime FechaHasta { get; set; }
+
+        public int Estado { get; set; }
+
+        [Display(Name = "Estado")]
+        public string NombreEstado { get; set; }
+
+
+        public string Observaciones { get; set; }
+
+        [Display(Name = "Vacaciones acumuladas")]
+        public int VacacionesAcumuladas { get; set; }
+    }
+}
