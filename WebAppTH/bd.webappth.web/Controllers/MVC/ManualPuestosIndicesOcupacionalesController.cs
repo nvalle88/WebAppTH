552fecdb00a5a1c852d4d521eba86a2e359c7fd9
jcatota <jcatota@digitalstrategy.com.ec>
2018-07-04 17:18:22 -0500

    Arreglado ManualPuestosIndicesOcupacionales (Distributivo)

--- WebAppTH/bd.webappth.web/Controllers/MVC/ManualPuestosIndicesOcupacionalesController.cs
+++ WebAppTH/bd.webappth.web/Controllers/MVC/ManualPuestosIndicesOcupacionalesController.cs
@@ -61,11 +61,15 @@ namespace bd.webappth.web.Controllers.MVC
         
         public async Task<IActionResult> Index()
         {
-            var lista = new List<IndiceOcupacionalViewModel>();
+            var lista = new List<IndiceOcupacionalModalidadPartida>();
+
             try
             {
-                lista = await apiServicio.Listar<IndiceOcupacionalViewModel>(new Uri(WebApp.BaseAddress)
-                                                                    , "api/IndicesOcupacionales/ListarIndicesOcupaciones");
+                lista = await apiServicio.Listar<IndiceOcupacionalModalidadPartida>(
+                    new Uri(WebApp.BaseAddress),
+                    "api/IndicesOcupacionalesModalidadPartida/MostrarDistributivo"
+                 );
+
                 return View(lista);
             }
             catch (Exception ex)
