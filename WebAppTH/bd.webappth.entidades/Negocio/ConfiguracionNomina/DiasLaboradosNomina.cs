c24ee3869346d32d62a79834876213ce4dd52975
Néstor Valle <nestorvalle880416@gmail.com>
2018-07-18 19:32:03 -0500

    se agrega relacion de días laborados

---
+++ WebAppTH/bd.webappth.entidades/Negocio/ConfiguracionNomina/DiasLaboradosNomina.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace bd.webappth.entidades.Negocio
+{
+    public class DiasLaboradosNomina
+    {
+        [Key]
+        public int IdDiasLaboradosNomina { get; set; }
+        public string IdentificacionEmpleado { get; set; }
+        public string NombreApellidoEmpleado  { get; set; }
+        public int CantidadDias { get; set; }
+
+        public int IdCalculoNomina { get; set; }
+        public virtual CalculoNomina CalculoNomina { get; set; }
+    }
+}
