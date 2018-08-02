d8b9a5d1317d960e1ab4bd224cea6514a089372c
jcatota <jcatota@digitalstrategy.com.ec>
2018-08-02 17:30:42 -0500

    activaciones tthh arreglado

--- WebAppTH/bd.webappth.entidades/ViewModels/ActivarPersonalTalentoHumanoViewModel.cs
+++ WebAppTH/bd.webappth.entidades/ViewModels/ActivarPersonalTalentoHumanoViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace bd.webappth.entidades.ViewModels
 {
@@ -11,6 +12,7 @@ namespace bd.webappth.entidades.ViewModels
 
         public int IdActivarPersonalTalentoHumano { get; set; }
         public int IdDependencia { get; set; }
+        public int IdSucursal { get; set; }
         public DateTime Fecha { get; set; }
         public bool Estado { get; set; }
         
@@ -18,6 +20,9 @@ namespace bd.webappth.entidades.ViewModels
         /* Campos de la tabla dependencia */
 
         public string Nombre { get; set; }
+
+        [Display(Name = "Área geográfica")]
+        public string SucursalNombre { get; set; }
 
         /* Uso: se llena true cuando existe un registro en el año actual */
         public bool Existe { get; set; }
